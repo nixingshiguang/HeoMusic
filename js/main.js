061d76fb97c88399ba4bdfd799301f03ef028884
nixingshiguang <nixingshiguang@qq.com>
2023-08-29 21:28:47 +0800

    change-some-css

--- js/main.js
+++ js/main.js
@@ -10,37 +10,37 @@ var heo = {
   changeMusicBg: function (isChangeBg = true) {
     const heoMusicBg = document.getElementById("music_bg")
 
-    if (isChangeBg) {
-      // player loadeddata 会进入此处
-      const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
-      var img = new Image();
-      img.src = extractValue(musiccover.style.backgroundImage);
-      img.onload = function() {
-        heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
-      };
-    } else {
+    // if (isChangeBg) {
+    //   // player loadeddata 会进入此处
+    //   const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
+    //   var img = new Image();
+    //   img.src = extractValue(musiccover.style.backgroundImage);
+    //   img.onload = function() {
+    //     heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+    //   };
+    // } else {
       // 第一次进入，绑定事件，改背景
-      let timer = setInterval(()=>{
-        const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
-        // 确保player加载完成
-        // console.info(heoMusicBg);
-        if (musiccover) {
-          clearInterval(timer)
-          //初始化音量
-          document.querySelector('meting-js').aplayer.volume(0.8,true);
-          // 绑定事件
-          heo.addEventListenerChangeMusicBg();
-        }
-      }, 100)
-    }
+      // let timer = setInterval(()=>{
+      //   const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
+      //   // 确保player加载完成
+      //   // console.info(heoMusicBg);
+      //   if (musiccover) {
+      //     clearInterval(timer)
+      //     //初始化音量
+      //     document.querySelector('meting-js').aplayer.volume(0.2,true);
+      //     // 绑定事件
+      //     heo.addEventListenerChangeMusicBg();
+      //   }
+      // }, 100)
+    // }
   },
-  addEventListenerChangeMusicBg: function () {
-    const heoMusicPage = document.getElementById("heoMusic-page");
-    heoMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
-      heo.changeMusicBg();
-      // console.info('player loadeddata');
-    });
-  },
+  // addEventListenerChangeMusicBg: function () {
+  //   const heoMusicPage = document.getElementById("heoMusic-page");
+  //   heoMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
+  //     heo.changeMusicBg();
+  //     // console.info('player loadeddata');
+  //   });
+  // },
   getCustomPlayList: function() {
     const heoMusicPage = document.getElementById("heoMusic-page");
     const playlistType = params.get("type") || "playlist";
@@ -72,11 +72,11 @@ window.addEventListener('resize', () => {
 });
 
 //获取图片url
-function extractValue(input) {
-  var valueRegex = /\("([^\s]+)"\)/g;
-  var match = valueRegex.exec(input);
-  return match[1];
-}
+// function extractValue(input) {
+//   var valueRegex = /\("([^\s]+)"\)/g;
+//   var match = valueRegex.exec(input);
+//   return match[1];
+// }
 
 //空格控制音乐
 document.addEventListener("keydown", function(event) {
