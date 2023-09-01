ac28e84509a2249a13088f652ca96ff93d7a1540
nixingshiguang <nixingshiguang@qq.com>
2023-09-01 14:10:39 +0800

    daily-update

--- js/main.js
+++ js/main.js
@@ -1,65 +1,12 @@
 console.log("\n %c HeoMusic 开源静态音乐播放器 v1.5 %c https://github.com/zhheo/HeoMusic \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
 var volume = 0.8;
-
-// 获取地址栏参数
-// 创建URLSearchParams对象并传入URL中的查询字符串
-const params = new URLSearchParams(window.location.search);
-
-var heo = {
-  // 音乐节目切换背景
-  changeMusicBg: function (isChangeBg = true) {
-    const heoMusicBg = document.getElementById("music_bg")
-
-    // if (isChangeBg) {
-    //   // player loadeddata 会进入此处
-    //   const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
-    //   var img = new Image();
-    //   img.src = extractValue(musiccover.style.backgroundImage);
-    //   img.onload = function() {
-    //     heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
-    //   };
-    // } else {
-    // 第一次进入，绑定事件，改背景
-    // let timer = setInterval(()=>{
-    //   const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
-    //   // 确保player加载完成
-    //   // console.info(heoMusicBg);
-    //   if (musiccover) {
-    //     clearInterval(timer)
-    //     //初始化音量
-    //     document.querySelector('meting-js').aplayer.volume(0.2,true);
-    //     // 绑定事件
-    //     heo.addEventListenerChangeMusicBg();
-    //   }
-    // }, 100)
-    // }
-  },
-  // addEventListenerChangeMusicBg: function () {
-  //   const heoMusicPage = document.getElementById("heoMusic-page");
-  //   heoMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
-  //     heo.changeMusicBg();
-  //     // console.info('player loadeddata');
-  //   });
-  // },
-  getCustomPlayList: function () {
-    const heoMusicPage = document.getElementById("heoMusic-page");
-    const playlistType = params.get("type") || "playlist";
-
-    if (params.get("id") && params.get("server")) {
-      console.log("获取到自定义内容")
-      var id = params.get("id")
-      var server = params.get("server")
-      heoMusicPage.innerHTML = `<meting-js id="${id}" server="${server}" type="${playlistType}" mutex="true" preload="auto" order="random"></meting-js>`;
-    } else {
-      console.log("无自定义内容")
-      heoMusicPage.innerHTML = `<meting-js id="${userId}" server="${userServer}" type="${userType}" mutex="true" preload="auto" order="random"></meting-js>`;
-    }
-    heo.changeMusicBg(false);
-  }
+function getPlayList () {
+  var heoMusicPage = document.getElementById("heoMusic-page");
+heoMusicPage.innerHTML = `<meting-js id="${userId}" server="${userServer}" type="${userType}" mutex="true" preload="auto" order="random"></meting-js>`;
 }
 
 // 调用
-heo.getCustomPlayList();
+getPlayList();
 //自定义
 //获取各种元素
 var change_list = document.getElementById("change-list");
@@ -70,7 +17,8 @@ var ci = document.getElementById("ci");
 var qu = document.getElementById("qu");
 var fanchang = document.getElementById("fanchang");
 //点击切换列表弹出选择
-change_list.addEventListener("click", () => {
+change_list.addEventListener("click", (event) => {
+  event.stopPropagation();
   if (second_level_list.style.display != "flex") {
     second_level_list.style.display = "flex";
   } else {
@@ -78,30 +26,35 @@ change_list.addEventListener("click", () => {
   }
 });
 
-quanbu.addEventListener("click", function () {
+quanbu.addEventListener("click", function (event) {
+  event.stopPropagation();
   userId = "7947315801";
   second_level_list.style.display = "none";
-  heo.getCustomPlayList();
+  getPlayList();
 });
-ciqu.addEventListener("click", function () {
+ciqu.addEventListener("click", function (event) {
+  event.stopPropagation();
   userId = "7801901402";
   second_level_list.style.display = "none";
-  heo.getCustomPlayList();
+  getPlayList();
 });
-ci.addEventListener("click", function () {
+ci.addEventListener("click", function (event) {
+  event.stopPropagation();
   userId = "7801915516";
   second_level_list.style.display = "none";
-  heo.getCustomPlayList();
+  getPlayList();
 });
-qu.addEventListener("click", function () {
+qu.addEventListener("click", function (event) {
+  event.stopPropagation();
   userId = "9008544992";
   second_level_list.style.display = "none";
-  heo.getCustomPlayList();
+  getPlayList();
 });
-fanchang.addEventListener("click", function () {
+fanchang.addEventListener("click", function (event) {
+  event.stopPropagation();
   userId = "7895783898";
   second_level_list.style.display = "none";
-  heo.getCustomPlayList();
+  getPlayList();
 });
 
 
@@ -113,13 +66,6 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 1;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-
-//获取图片url
-// function extractValue(input) {
-//   var valueRegex = /\("([^\s]+)"\)/g;
-//   var match = valueRegex.exec(input);
-//   return match[1];
-// }
 
 //空格控制音乐
 document.addEventListener("keydown", function (event) {
